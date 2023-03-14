9b0b109c862b6018922f6153b350da838ddd4ae6
Shaked Asido <shakedasido@gmail.com>
2023-03-14 22:07:11 +0200

    Wrote Client 1's side: Client 1 sends her\his name and an address of the server, and the server inserts them into a names&addresses dictionary. Then the client sends another message with the name of the other client (client 2) s\he wants to send a message to and the message, superheated with spaces. if client 2 appears in the names at the dictionary, the server sends the message client 1 sent to client 2. else: client 1 will get a message say's: "no such user".

--- Client1Side/Client1.py
+++ Client1Side/Client1.py
@@ -8,12 +8,11 @@ BUFFER_SIZE = 1024
 client1_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, 0)
 my_name = input('Enter your name: ')
 client1_sock.sendto(my_name.encode(), SERVER_ADDR)
-print(1)
+print("Enter the name of the person you wants to send a message to, and the message suppurated with spaces, Please:")
 
 
 def output_recvfrom(sock):
     while True:
-        print(2)
         data, _ = sock.recvfrom(BUFFER_SIZE)
         if not data: break
         print(data.decode())
@@ -22,7 +21,6 @@ def output_recvfrom(sock):
 x = Thread(target=output_recvfrom, args=(client1_sock,))
 x.start()
 for line in sys.stdin:
-    print(3)
     client1_sock.sendto(line.strip().encode(), SERVER_ADDR)
 client1_sock.close()
 x.join()
